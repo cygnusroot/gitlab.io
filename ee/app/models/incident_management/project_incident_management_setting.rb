db253bda48c4371d6652566a31d02004bd45a61a
Peter Leitzen <pleitzen@gitlab.com>
2019-02-28 15:55:28 +0100

    Expose issue template's content

--- ee/app/models/incident_management/project_incident_management_setting.rb
+++ ee/app/models/incident_management/project_incident_management_setting.rb
@@ -2,9 +2,17 @@
 
 module IncidentManagement
   class ProjectIncidentManagementSetting < ApplicationRecord
+    include Gitlab::Utils::StrongMemoize
+
     belongs_to :project
 
     validate :issue_template_exists, if: :create_issue?
+
+    def issue_template_content
+      strong_memoize(:issue_template_content) do
+        load_issue_template_content
+      end
+    end
 
     private
 
@@ -14,6 +22,13 @@ module IncidentManagement
       Gitlab::Template::IssueTemplate.find(issue_template_key, project)
     rescue Gitlab::Template::Finders::RepoTemplateFinder::FileNotFoundError
       errors.add(:issue_template_key, 'not found')
+    end
+
+    def load_issue_template_content
+      return unless issue_template_key.present?
+
+      Gitlab::Template::IssueTemplate.find(issue_template_key, project).content
+    rescue Gitlab::Template::Finders::RepoTemplateFinder::FileNotFoundError
     end
   end
 end
